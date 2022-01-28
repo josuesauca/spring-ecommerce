6dd2c49275ef4c5206f1156553084bd78a2f6b0a
[josuesauca <josuesauca@gmail.com>
2022-01-27 20:29:26 -0500

    Funcionalidad para editar productos

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -7,14 +7,12 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "productos")
